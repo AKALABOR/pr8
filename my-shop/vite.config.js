2990b42751e6cf45bbbb27353e022ce8e9a724d7
AKALABOR <110409066+AKALABOR@users.noreply.github.com>
2026-05-26 11:42:15 +0300

    Update vite.config.js

--- my-shop/vite.config.js
+++ my-shop/vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pr8/'
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
